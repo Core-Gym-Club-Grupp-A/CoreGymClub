b591b4162a6d81347616d1dbb799ad0dea3a851d
FredricDomvall <Fredric.domvall@outlook.com>
2025-11-12 23:30:44 +0100

    user story 1 - interface - trainingsessionrepository

---
+++ Domain/Interfaces/ITrainingSessionRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Models;
+
+namespace Domain.Interfaces;
+
+/* 
+    User Story 1: Som användare vill jag kunna se en lista över kommande träningspass.
+    Detta interface ansvarar för dataåtkomst. 
+    Här definieras anropet som hämtar träningspass från datakällan (t.ex. databas eller mock-data).
+*/
+
+public interface ITrainingSessionRepository
+{
+    Task<IEnumerable<TrainingSession>> GetAllSessionsAsync();
+}
